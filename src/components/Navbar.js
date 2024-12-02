21f0a53d813266822315e0cca140d78a9059c629
felbiero <felbiero@gmail.com>
2024-12-02 16:14:38 +0300

    HomePage - table build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import { FaUserCircle } from "react-icons/fa";
 
 import Home from "../pages/Home";
 import Fixtures from "../pages/Fixture";
@@ -12,11 +13,14 @@ import Dashboard from "../pages/Dashboard";
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container">
-          {/* <Link to="/" className="nav-logo">
-            <img src={logo} alt="Logo" />
-          </Link> */}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navNavbar">
+        <div className="container container-content">
+          <Link className="navbar-brand navar-logo" href="#">
+            <img
+              src={window.location.origin + "../assets/images/fkf-logo.png"}
+            />
+            Navbar
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -29,23 +33,32 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-            <div className="d-flex">
-              <Link element={<Home />} to="/">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <Link element={<Home />} to="/" className="nav-link">
                 Home
               </Link>
-              <Link element={<Teams />} to="/teams">
+              <Link element={<Teams />} to="/teams" className="nav-link">
                 Clubs
               </Link>
-              <Link element={<Fixtures />} to="/fixtures">
+              <Link element={<Fixtures />} to="/fixtures" className="nav-link">
                 Fixtures
               </Link>
-              <Link element={<Standings />} to="/standings">
+              <Link
+                element={<Standings />}
+                to="/standings"
+                className="nav-link"
+              >
                 Standings
               </Link>
+            </ul>
+            <div className="d-flex">
               {/* login User  */}
-              <Link element={<Dashboard />} to="/dashboard">
-                Login
+              <Link
+                element={<Dashboard />}
+                to="/dashboard"
+                className="nav-link nav-link-user"
+              >
+                <FaUserCircle />
               </Link>
             </div>
           </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,253 @@
 import React from "react";
 
+import { FaArrowRightLong } from "react-icons/fa6";
+
+import { Link } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
+import Standings from "./Standings";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="container">LandingPage</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="main">main</div>
+          </div>
+          <div className="col-md-4">
+            <div className="right-aside">
+              <div className="league-table">
+                <div className="table-header">
+                  <div className="table-head">
+                    <h3>Regional League</h3>
+                  </div>
+                </div>
+                <div className="table-data">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Club</th>
+                        <th scope="col">W</th>
+                        <th scope="col">D</th>
+                        <th scope="col">L</th>
+                        <th scope="col">Pts</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>TeamOne</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>TeamTwo</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>TeamThree</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">4</th>
+                        <td>Four</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">5</th>
+                        <td>Five</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <Link
+                    element={<Standings />}
+                    to="/standings"
+                    className="nav-link-standing-btn"
+                  >
+                    Load More
+                  </Link>
+                </div>
+              </div>
+
+              {/* COUNTY LEAGUE  */}
+              <div className="league-table">
+                <div className="table-header">
+                  <div className="table-head">
+                    <h3>county League</h3>
+                  </div>
+                </div>
+                <div className="table-data">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Club</th>
+                        <th scope="col">W</th>
+                        <th scope="col">D</th>
+                        <th scope="col">L</th>
+                        <th scope="col">Pts</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>TeamOne</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>TeamTwo</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>TeamThree</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">4</th>
+                        <td>Four</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">5</th>
+                        <td>Five</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <Link
+                    element={<Standings />}
+                    to="/standings"
+                    className="nav-link-standing-btn"
+                  >
+                    Load More
+                  </Link>
+                </div>
+              </div>
+
+              {/* SU-COUNTY LEAGUE  */}
+              <div className="league-table">
+                <div className="table-header">
+                  <div className="table-head">
+                    <h3>sub-county League</h3>
+                  </div>
+                </div>
+                <div className="table-data">
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Club</th>
+                        <th scope="col">W</th>
+                        <th scope="col">D</th>
+                        <th scope="col">L</th>
+                        <th scope="col">Pts</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>TeamOne</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>TeamTwo</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>TeamThree</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">4</th>
+                        <td>Four</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+
+                      <tr>
+                        <th scope="row">5</th>
+                        <td>Five</td>
+                        <td>10</td>
+                        <td>3</td>
+                        <td>5</td>
+                        <td>45</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <Link
+                    element={<Standings />}
+                    to="/standings"
+                    className="nav-link-standing-btn"
+                  >
+                    Load More
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
